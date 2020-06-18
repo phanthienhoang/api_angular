f3eab021b72aebe5e7ef6f24d85201db73da9e2e
phanthienhoang95 <phanthienhoang95@gmail.com>
2020-06-18 17:21:54 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::resource('post', 'ApiPostController')->middlewware('core');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::resource('post', 'ApiPostController');
