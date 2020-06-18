79c325acab79612175d1a8266522b1163ff1fdf4
phanthienhoang95 <phanthienhoang95@gmail.com>
2020-06-18 17:26:18 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('post', 'ApiPostController');
